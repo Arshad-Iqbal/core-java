97d4ede36708c2a8499534b143c5e4edbbd26961
arshadullah <arshadullah@boomerangcommerce.com>
2019-07-13 15:21:41 +0530

    adding Worker Timer thread task example

--- multithreading/src/main/java/com/arshad/java/tutorials/multithreading/LongWrapper.java
+++ multithreading/src/main/java/com/arshad/java/tutorials/multithreading/LongWrapper.java
@@ -9,7 +9,22 @@ public class LongWrapper {
 	public LongWrapper(long l) {
 		this.l = l;
 	}
-
+	
+	/**
+	 * 
+	 * Need to observe the getValue(GV) and incrementValue(IV) closely.
+	 * This code is buggy, Though it runs correctly in dev environment 
+	 * here, the write is synchronize GV but the read GV is neither synchronized nor volatile, So it does not 
+	 *       offer the guarantee to return the last value written in the IV method.
+	 * If we want to make this correct, We need to also synchronize the block of code in GV() like below,
+	 * 
+	 * synchronized(key){
+	 * 		return l;
+	 * }
+	 * conclusion: The happens-before link was missing in the previous example.
+	 *   
+	 * @return
+	 */
 	public long getValue() {
 		return l;
 	}
@@ -20,4 +35,34 @@ public class LongWrapper {
 		}
 	}
 
+	
+	//Testing LongWrapper for race condition
+	
+	public static void main(String[] args) throws InterruptedException {
+		LongWrapper longWrapper = new LongWrapper(0L);
+		
+		Runnable r = () -> {
+			for(int i = 0; i < 1_000; i++) {
+				longWrapper.incrementValue();
+			}
+		};
+		
+		Thread[] threads = new Thread[1_000]; 
+		for(int i = 0; i < threads.length; i++) {
+			threads[i] = new Thread(r);
+			threads[i].start();
+		}
+		/*Thread t = new Thread(r);
+		t.start();
+		
+		t.join(); // Ensures the code after this line will be executed 
+				  // after the Thread t execution finishes.
+		*/
+		
+		for(int i = 0; i < threads.length; i++) {
+			threads[i].join();
+		}
+		
+		System.out.println("Value = "+longWrapper.getValue());
+	}
 }
